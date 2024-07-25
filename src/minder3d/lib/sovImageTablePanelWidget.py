ae2e4501231ece394953c72a0949788355def3f6
Stephen Aylward <stephen@aylward.org>
2024-07-25 11:48:08 -0400

    ENH: Provide a column/action for selecting images/scenes in list
    Before selecting == loading.  Now items can be selected without
    loading and loaded items can be unloaded.

--- src/minder3d/lib/sovImageTablePanelWidget.py
+++ src/minder3d/lib/sovImageTablePanelWidget.py
@@ -7,6 +7,7 @@ from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import (
     QHeaderView,
+    QStyle,
     QTableWidget,
     QTableWidgetItem,
     QWidget,
@@ -36,11 +37,25 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
 
         self.settings = ImageTableSettings()
 
+        self.selected = []
+        pixmapi = getattr(QStyle, 'SP_DialogApplyButton')
+        self.selected_icon = self.style().standardIcon(pixmapi)
+
         self.imageTableWidget.setRowCount(0)
-        self.imageTableWidget.setColumnCount(6)
+        self.imageTableWidget.setColumnCount(7)
         self.imageTableWidget.setHorizontalHeaderLabels(
-            ['Loaded', 'Type', 'Thumbnail', 'Filename', 'Size', 'Spacing']
+            [
+                'Selected',
+                'Loaded',
+                'Type',
+                'Thumbnail',
+                'Size',
+                'Spacing',
+                'Filename',
+            ]
         )
+        self.col_filetype = 2
+        self.col_filename = 6
         self.imageTableWidget.setEditTriggers(QTableWidget.NoEditTriggers)
         self.imageTableWidget.setSelectionBehavior(QTableWidget.SelectRows)
         self.imageTableWidget.setSelectionMode(QTableWidget.SingleSelection)
@@ -59,8 +74,10 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
 
         self.imageTableWidget.cellClicked.connect(self.select_data_by_table)
 
-        self.imageTableUnloadButton.clicked.connect(self.unload_image)
-        self.imageTableRemoveButton.clicked.connect(self.remove_image)
+        self.imageTableLoadButton.clicked.connect(self.load_selected)
+        self.imageTableUnloadButton.clicked.connect(self.unload_selected)
+        self.imageTableRenameButton.clicked.connect(self.rename_selected)
+        self.imageTableRemoveButton.clicked.connect(self.remove_selected)
         self.imageTableRemoveAllButton.clicked.connect(self.remove_all)
         self.imageTableExpandButton.clicked.connect(self.expand_table)
 
@@ -69,22 +86,43 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
         self.fill_table()
 
     @time_and_log
-    def unload_image(self):
-        self.gui.unload_image(self.state.current_image_num, False)
-        self.fill_table()
+    def unload_selected(self):
+        for row in self.selected:
+            if row < len(self.state.image_filename):
+                self.gui.unload_image(row, False)
+            elif row == len(self.state.image_filename):
+                self.gui.unload_scene()
+        if len(self.selected) > 0:
+            self.selected = []
+            self.fill_table()
 
-    def remove_image(self):
-        self.settings.remove_data(
-            self.state.image_filename[self.state.current_image_num]
-        )
-        self.gui.unload_image(self.state.current_image_num, False)
-        self.fill_table()
+    def remove_selected(self):
+        for row in self.selected:
+            if row < len(self.state.image_filename):
+                self.gui.unload_image(row, False)
+                self.settings.remove_data(self.state.image_filename[row])
+            elif row == len(self.state.image_filename):
+                self.gui.unload_scene()
+                self.settings.remove_data(self.state.scene_filename)
+            else:
+                filename = str(
+                    self.imageTableWidget.getItem(
+                        row, self.col_filename
+                    ).values[0]
+                )
+                self.settings.remove_data(filename)
+        if len(self.selected) > 0:
+            self.selected = []
+            self.fill_table()
 
     def remove_all(self):
         num_images = len(self.state.image_filename)
-        for i in range(num_images - 1, 0, -1):
-            self.settings.remove_data(self.state.image_filename[i])
-        self.gui.unload_image(self.state.current_image_num, False)
+        for row in range(num_images - 1, 0, -1):
+            self.gui.unload_image(row, False)
+            self.settings.remove_data(self.state.image_filename[row])
+        self.gui.unload_scene()
+        self.settings.remove_data(self.state.scene_filename)
+        self.selected = []
         self.settings.clear_data()
         self.fill_table()
 
@@ -108,12 +146,16 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
 
     @time_and_log
     def update_image(self):
+        self.selected = []
         self.fill_table()
+        # Probably need to disable selection callback when calling this function
         self.imageTableWidget.selectRow(self.state.current_image_num)
 
     @time_and_log
     def update_scene(self):
+        self.selected = []
         self.fill_table()
+        # Probably need to disable selection callback when calling this function
         self.imageTableWidget.selectRow(self.state.current_image_num)
 
     @time_and_log
@@ -133,22 +175,35 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
         """
 
         img_num = row_num
-        if img_num < len(self.state.image_filename):
+        col_num = 0
+        if row_num in self.selected:
             self.imageTableWidget.setItem(
-                row_num, 0, QTableWidgetItem(str('X'))
+                row_num, col_num, QTableWidgetItem(self.selected_icon, '')
             )
         else:
-            self.imageTableWidget.setItem(row_num, 0, QTableWidgetItem(str('')))
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem('')
+            )
+        col_num += 1
+        if img_num < len(self.state.image_filename):
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem(self.selected_icon, '')
+            )
+        else:
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem('')
+            )
+        col_num += 1
         self.imageTableWidget.setItem(
-            row_num, 1, QTableWidgetItem(str('Image'))
+            row_num, col_num, QTableWidgetItem(str('Image'))
         )
+        col_num += 1
         self.imageTableWidget.setItem(
             row_num,
-            2,
+            col_num,
             QTableWidgetItem(QIcon(self.state.image_thumbnail[img_num]), ''),
         )
-        filename = self.state.image_filename[img_num]
-        self.imageTableWidget.setItem(row_num, 3, QTableWidgetItem(filename))
+        col_num += 1
         size_str = [
             str(i)
             for i in self.state.image[img_num]
@@ -156,39 +211,68 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             .GetSize()
         ]
         self.imageTableWidget.setItem(
-            row_num, 4, QTableWidgetItem('x'.join(size_str))
+            row_num, col_num, QTableWidgetItem('x'.join(size_str))
         )
+        col_num += 1
         spacing_str = [
             f'{i:.4f}' for i in self.state.image[img_num].GetSpacing()
         ]
         self.imageTableWidget.setItem(
-            row_num, 5, QTableWidgetItem(','.join(spacing_str))
+            row_num, col_num, QTableWidgetItem(','.join(spacing_str))
+        )
+        col_num += 1
+        filename = self.state.image_filename[img_num]
+        self.imageTableWidget.setItem(
+            row_num, col_num, QTableWidgetItem(filename)
         )
 
     @time_and_log
-    def redraw_scene_row(self, row_num, selected=False):
-        if selected or (
+    def redraw_scene_row(self, row_num):
+        col_num = 0
+        if row_num in self.selected:
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem(self.selected_icon, '')
+            )
+        else:
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem('')
+            )
+        col_num += 1
+        if (
             self.imageTableWidget.item(row_num, 3) is not None
             and self.imageTableWidget.item(row_num, 3).text()
             == self.state.scene_filename
         ):
             self.imageTableWidget.setItem(
-                row_num, 0, QTableWidgetItem(str('X'))
+                row_num, col_num, QTableWidgetItem(self.selected_icon, '')
             )
         else:
-            self.imageTableWidget.setItem(row_num, 0, QTableWidgetItem(str('')))
+            self.imageTableWidget.setItem(
+                row_num, col_num, QTableWidgetItem('')
+            )
+        col_num += 1
         self.imageTableWidget.setItem(
-            row_num, 1, QTableWidgetItem(str('Scene'))
+            row_num, col_num, QTableWidgetItem(str('Scene'))
         )
+        col_num += 1
         self.imageTableWidget.setItem(
-            row_num, 2, QTableWidgetItem(QIcon(self.state.scene_thumbnail), '')
+            row_num,
+            col_num,
+            QTableWidgetItem(QIcon(self.state.scene_thumbnail), ''),
         )
-        self.imageTableWidget.setItem(
-            row_num, 3, QTableWidgetItem(str(self.state.scene_filename))
-        )
+        col_num += 1
         size = self.state.scene.GetNumberOfChildren()
-        self.imageTableWidget.setItem(row_num, 4, QTableWidgetItem(str(size)))
-        self.imageTableWidget.setItem(row_num, 5, QTableWidgetItem(str('')))
+        self.imageTableWidget.setItem(
+            row_num, col_num, QTableWidgetItem(str(size))
+        )
+        col_num += 1
+        self.imageTableWidget.setItem(
+            row_num, col_num, QTableWidgetItem(str('N/A'))
+        )
+        col_num += 1
+        self.imageTableWidget.setItem(
+            row_num, col_num, QTableWidgetItem(str(self.state.scene_filename))
+        )
 
     @time_and_log
     def fill_table(self):
@@ -207,7 +291,15 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
         self.imageTableWidget.clear()
         self.imageTableWidget.setRowCount(0)
         self.imageTableWidget.setHorizontalHeaderLabels(
-            ['Loaded', 'Type', 'Thumbnail', 'Filename', 'Size', 'Spacing']
+            [
+                'Selected',
+                'Loaded',
+                'Type',
+                'Thumbnail',
+                'Size',
+                'Spacing',
+                'Filename',
+            ]
         )
         row_num = 0
         for _ in range(len(self.state.image)):
@@ -216,7 +308,7 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             row_num += 1
         if self.state.scene.GetNumberOfChildren() > 0:
             self.imageTableWidget.insertRow(row_num)
-            self.redraw_scene_row(row_num, selected=True)
+            self.redraw_scene_row(row_num)
             row_num += 1
 
         file_records = self.settings.get_file_records()
@@ -226,22 +318,47 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
                 and file.file_type == 'image'
             ):
                 self.imageTableWidget.insertRow(row_num)
+                col_num = 0
+                if row_num in self.selected:
+                    self.imageTableWidget.setItem(
+                        row_num,
+                        col_num,
+                        QTableWidgetItem(self.selected_icon, ''),
+                    )
+                else:
+                    self.imageTableWidget.setItem(
+                        row_num, col_num, QTableWidgetItem('')
+                    )
+                col_num += 1
+                loaded_text = ' '
                 self.imageTableWidget.setItem(
-                    row_num, 1, QTableWidgetItem('Image')
+                    row_num, col_num, QTableWidgetItem(loaded_text)
                 )
+                col_num += 1
+                self.imageTableWidget.setItem(
+                    row_num, col_num, QTableWidgetItem('Image')
+                )
+                col_num += 1
                 if file.file_thumbnail != '':
                     qthumb = QPixmap(file.file_thumbnail)
                     self.imageTableWidget.setItem(
-                        row_num, 2, QTableWidgetItem(QIcon(qthumb), '')
+                        row_num, col_num, QTableWidgetItem(QIcon(qthumb), '')
                     )
+                else:
+                    self.imageTableWidget.setItem(
+                        row_num, col_num, QTableWidgetItem(' ')
+                    )
+                col_num += 1
                 self.imageTableWidget.setItem(
-                    row_num, 3, QTableWidgetItem(str(file.filename))
+                    row_num, col_num, QTableWidgetItem(str(file.file_size))
                 )
+                col_num += 1
                 self.imageTableWidget.setItem(
-                    row_num, 4, QTableWidgetItem(str(file.file_size))
+                    row_num, col_num, QTableWidgetItem(str(file.file_spacing))
                 )
+                col_num += 1
                 self.imageTableWidget.setItem(
-                    row_num, 5, QTableWidgetItem(str(file.file_spacing))
+                    row_num, col_num, QTableWidgetItem(str(file.filename))
                 )
                 row_num += 1
             elif (
@@ -249,18 +366,42 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
                 and file.file_type == 'scene'
             ):
                 self.imageTableWidget.insertRow(row_num)
+                col_num = 0
+                if row_num in self.selected:
+                    self.imageTableWidget.setItem(
+                        row_num,
+                        col_num,
+                        QTableWidgetItem(self.selected_icon, ''),
+                    )
+                else:
+                    self.imageTableWidget.setItem(
+                        row_num, col_num, QTableWidgetItem('')
+                    )
+                col_num += 1
+                loaded_text = ' '
                 self.imageTableWidget.setItem(
-                    row_num, 1, QTableWidgetItem('Scene')
+                    row_num, col_num, QTableWidgetItem(loaded_text)
                 )
+                col_num += 1
+                self.imageTableWidget.setItem(
+                    row_num, col_num, QTableWidgetItem('Scene')
+                )
+                col_num += 1
                 qthumb = QPixmap(file.file_thumbnail)
                 self.imageTableWidget.setItem(
-                    row_num, 2, QTableWidgetItem(QIcon(qthumb), '')
+                    row_num, col_num, QTableWidgetItem(QIcon(qthumb), '')
                 )
+                col_num += 1
                 self.imageTableWidget.setItem(
-                    row_num, 3, QTableWidgetItem(file.filename)
+                    row_num, col_num, QTableWidgetItem(file.file_size)
                 )
+                col_num += 1
                 self.imageTableWidget.setItem(
-                    row_num, 4, QTableWidgetItem(file.file_size)
+                    row_num, col_num, QTableWidgetItem('N/A')
+                )
+                col_num += 1
+                self.imageTableWidget.setItem(
+                    row_num, col_num, QTableWidgetItem(file.filename)
                 )
                 row_num += 1
 
@@ -277,6 +418,7 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             'image',
             self.state.image_thumbnail[-1],
         )
+        self.selected = []
         self.fill_table()
 
     @time_and_log
@@ -287,6 +429,7 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             'image',
             self.state.image_thumbnail[self.state.current_image_num],
         )
+        self.selected = []
         self.fill_table()
 
     @time_and_log
@@ -300,6 +443,7 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             'scene',
             self.state.scene_thumbnail,
         )
+        self.selected = []
         self.fill_table()
 
     @time_and_log
@@ -312,26 +456,48 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
     def replace_image(self, img_num):
         self.redraw_image_row(img_num)
 
+    def rename_selected(self):
+        pass
+
     @time_and_log
-    def select_data_by_table(self, row, _):
+    def select_data_by_table(self, row, col):
+        if col == 0:
+            if row in self.selected:
+                self.selected.remove(row)
+            else:
+                self.selected.append(row)
+            self.fill_table()
+            return
         if row < len(self.state.image_filename):
             self.state.current_image_num = row
             self.gui.update_image()
             if self.state.view2D_overlay_auto_update:
                 self.gui.update_overlay()
-        elif (
-            row > 0
-            and row == len(self.state.image_filename)
-            and self.state.scene_filename
-            == self.imageTableWidget.item(row, 3).text()
-        ):
-            # current scene
-            return
-        else:
-            if self.imageTableWidget.item(row, 1).text() == 'Image':
-                self.gui.load_image(self.imageTableWidget.item(row, 3).text())
-            elif self.imageTableWidget.item(row, 1).text() == 'Scene':
-                self.gui.load_scene(self.imageTableWidget.item(row, 3).text())
+
+    @time_and_log
+    def load_selected(self):
+        for row in self.selected:
+            if row >= len(self.state.image_filename):
+                if (
+                    self.imageTableWidget.item(row, self.col_filetype).text()
+                    == 'Image'
+                ):
+                    self.gui.load_image(
+                        self.imageTableWidget.item(
+                            row, self.col_filename
+                        ).text()
+                    )
+                elif (
+                    self.imageTableWidget.item(row, self.col_filetype).text()
+                    == 'Scene'
+                ):
+                    self.gui.load_scene(
+                        self.imageTableWidget.item(
+                            row, self.col_filename
+                        ).text()
+                    )
+        self.selected = []
+        self.fill_table()
 
     @time_and_log
     def register_images(self, dir, first=True):
@@ -373,4 +539,5 @@ class ImageTablePanelWidget(QWidget, Ui_ImageTablePanelWidget):
             self.register_images(dirname, False)
 
         if first:
+            self.selected = []
             self.fill_table()
